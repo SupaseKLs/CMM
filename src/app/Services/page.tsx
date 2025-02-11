73dfa7ed6902671f132caab7fea060595995048b
SupaseKLs <estzaz35@gmail.com>
2025-02-11 17:55:28 +0700

    test cloud to keep image and video in azure

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -1,6 +1,16 @@
 export default function Services() {
     return(
         <>
+        <img src="https://executivemembers.file.core.windows.net/executive/Screenshot%202025-02-06%20133214.png?sp=r&st=2025-02-11T10:06:51Z&se=2025-02-12T10:06:51Z&sv=2022-11-02&sig=5knQsyutevgN21%2B8X%2BwhvC3YTMDw3LkJuNmufmu2BVk%3D&sr=f" alt="" />
+
+        <video
+                            playsInline
+                            autoPlay
+                            muted
+                            loop
+                        >
+                            <source src="https://executivemembers.file.core.windows.net/executive/2887463-hd_1920_1080_25fps.mp4?sp=r&st=2025-02-11T10:41:53Z&se=2030-02-12T10:41:00Z&sv=2022-11-02&sig=hYGanDs8HU0L9%2FEcwboVA2kQ77jSbdJcGldZGQCwugk%3D&sr=f" type="video/mp4" />
+                        </video>
         </>
     )
 }
